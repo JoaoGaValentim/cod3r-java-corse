d081d71d0e0ad630e19c1b8b522007e463dfba1f
João Gabriel Valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-09-21 21:58:05 -0300

    Added session two

---
+++ src/session/two/IfControlStructure.java
@@ -0,0 +1,31 @@
+package session.two;
+
+import java.util.Scanner;
+
+public class IfControlStructure {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe sua idade: ");
+        var age = sc.nextInt();
+
+        System.out.println("idade: " + age);
+
+        var isLegalAge = age >= 18;
+        var isChildren = age >= 5 && age <= 10;
+        var isTeen = age >= 11 && age <= 17;
+        if (isLegalAge) {
+            System.out.println("Você é maior de idade.");
+        }
+
+        if (isChildren) {
+            System.out.println("Você é uma criança.");
+        }
+
+        if (isTeen) {
+            System.out.println("Você é um adolescente.");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/session/two/IfTest.java
@@ -0,0 +1,12 @@
+package session.two;
+
+public class IfTest {
+    public static void main(String[] args) {
+        double point = 1.3;
+
+        if (point >= 9.0) {
+            System.out.println("Quadro de honra!");
+            System.out.println("Você é fera!");
+        }
+    }
+}
