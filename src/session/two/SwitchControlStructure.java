850c699d3068135c00ec29a70323707ddd5fb27a
João Gabriel Valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-09-26 12:51:21 -0300

    modifications in .java class

--- src/session/two/SwitchControlStructure.java
+++ src/session/two/SwitchControlStructure.java
@@ -12,27 +12,21 @@ public class SwitchControlStructure {
             case 2:
                 System.out.println("Terça");
                 break;
-
             case 3:
                 System.out.println("Quarta");
                 break;
-
             case 4:
                 System.out.println("Quinta");
                 break;
-
             case 5:
                 System.out.println("Sexta");
                 break;
-
             case 6:
                 System.out.println("Sábado");
                 break;
-
             case 7:
                 System.out.println("Domingo");
                 break;
-
             default:
                 System.out.println("Dia inválido");
                 break;
