88363df7bcd310521a939eb8860e5a8cc035f9fd
João Gabriel Valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-10-03 12:15:43 -0300

    added session #4

--- src/session/four/MatrixArray.java
+++ src/session/four/MatrixArray.java
@@ -1,30 +1,32 @@
 package session.four;
 
+import java.util.Scanner;
+
 public class MatrixArray {
     public static void main(String[] args) {
-        double[][] numericsMatrixA = new double[][] {
-                { 1, 3, 5 },
-                { 7, 9, 11 }
-        };
+        Scanner sc = new Scanner(System.in);
 
-        double[][] numericsMatrixB = new double[][] {
-                { 2, 4, 6 },
-                { 8, 10, 12 }
-        };
+        System.out.println("Informe o total de alunos da sala: ");
+        int studentsTotalInClass = sc.nextInt(); // linhas
 
-        double[][] resultMatrix = new double[2][3];
+        System.out.println("Quantas notas tem cada aluno? ");
+        int mentionsTotalPerStudent = sc.nextInt(); // colunas
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                resultMatrix[i][j] += numericsMatrixA[i][j] + numericsMatrixB[i][j];
+        double[][] studentsMention = new double[studentsTotalInClass][mentionsTotalPerStudent];
+        double total = 0.0;
+
+        for (int i = 0; i < studentsMention.length; i++) { // i -> rows
+            for (int j = 0; j < studentsMention[i].length; j++) { // j -> columns
+                System.out.println("Informe a " + (j + 1) + " nota do aluno " + (i + 1) + ":");
+                studentsMention[i][j] = sc.nextDouble();
+                total += studentsMention[i][j];
             }
         }
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.printf(" %s ", resultMatrix[i][j]);
-            }
-            System.err.println();
-        }
+        final double average = total / studentsMention.length;
+
+        System.out.printf("Média da turma = %.2f\n", average);
+
+        sc.close();
     }
 }
