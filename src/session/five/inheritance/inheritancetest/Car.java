02b275dc6d49929545e006984be3e1d08039ed2c
João Gabriel Valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-10-04 23:38:15 -0300

    add new session #5

--- src/session/five/inheritance/inheritancetest/Car.java
+++ src/session/five/inheritance/inheritancetest/Car.java
@@ -1,17 +1,16 @@
 package session.five.inheritance.inheritancetest;
 
 public class Car {
+    final double MAX_VELOCITY;
+    double delta = 5.0;
     double actualSpeed;
     boolean carIsOn;
     boolean isBreak;
 
-    Car() {
-        this(false);
-    }
-
-    Car(boolean carIsOn) {
+    Car(boolean carIsOn, double MAX_VELOCITY) {
         this.carIsOn = carIsOn;
         this.isBreak = false;
+        this.MAX_VELOCITY = MAX_VELOCITY;
     }
 
     void turnOnCar() {
@@ -27,6 +26,10 @@ public class Car {
     double accelerate() {
         if (actualSpeed == 0.0 && !carIsOn || isBreak) {
             return actualSpeed;
+        }
+
+        if (actualSpeed + delta > MAX_VELOCITY) {
+            actualSpeed = MAX_VELOCITY;
         }
 
         if (carIsOn && !isBreak || actualSpeed == 0.0) {
--- src/session/five/inheritance/inheritancetest/Ferrari.java
+++ src/session/five/inheritance/inheritancetest/Ferrari.java
@@ -1,10 +1,23 @@
 package session.five.inheritance.inheritancetest;
 
 public class Ferrari extends Car {
+
+    Ferrari() {
+        super(false, 310);
+    }
+
+    Ferrari(boolean carIsOn, double MAX_VELOCITY) {
+        super(carIsOn, MAX_VELOCITY);
+    }
+
     @Override
     double accelerate() {
         if (actualSpeed == 0.0 && !carIsOn || isBreak) {
             return actualSpeed;
+        }
+
+        if (actualSpeed + delta > MAX_VELOCITY) {
+            actualSpeed = MAX_VELOCITY;
         }
 
         if (carIsOn && !isBreak || actualSpeed == 0.0) {
