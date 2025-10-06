b70974872a9bbb596530e34faca4c7171a5dedb2
João Gabriel Valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-10-06 18:35:50 -0300

    add new session #6

--- src/session/six/FunctionJava.java
+++ src/session/six/FunctionJava.java
@@ -3,13 +3,14 @@ package session.six;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FunctionJava {
     public static void main(String[] args) {
         List<Product> products = Arrays.asList(
                 new Product("Arroz", 22, 12, 0.2),
                 new Product("Feijão", 24, 14, 0.3),
-                new Product("Pizza", 33, 20, 0.1));
+                new Product("Pizza", 40, 20, 0.1));
 
         Function<String, Product> getProductByName = name -> {
             for (Product product : products) {
@@ -21,11 +22,14 @@ public class FunctionJava {
         };
 
         Function<Product, Double> computePrice = p -> p == null ? 0.0 : p.getQuantity() * p.getPrice();
+        Predicate<Double> isHighPrice = price -> price >= 750 ? true : false;
+        Function<Double, Boolean> verifyHighPrice = price -> isHighPrice.test(price);
 
-        var total = getProductByName
+        var isPizzaHighPrice = getProductByName
                 .andThen(computePrice)
+                .andThen(verifyHighPrice)
                 .apply("pizza");
 
-        System.out.println(total);
+        System.out.println(isPizzaHighPrice);
     }
 }
