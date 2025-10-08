ff5f2b463ec89fec69696bbaa8e2d1d1f91f67e1
João Gabriel Valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-10-08 17:49:42 -0300

    finished session #7

--- src/session/seven/Student.java
+++ src/session/seven/Student.java
@@ -16,4 +16,32 @@ public class Student {
     public double getAverage() {
         return average;
     }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (name == null) ? 0 : name.hashCode();
+    }
 }
