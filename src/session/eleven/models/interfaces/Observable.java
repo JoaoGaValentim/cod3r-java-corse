dd181e97767cf17cbb5a59c1621331973a0df2bc
João Gabriel Valentim Theodoro <joao.gabriel.vale.theodoro@gmail.com>
2025-10-24 18:47:56 -0300

    refined module

--- src/session/eleven/models/interfaces/Observable.java
+++
@@ -1,9 +0,0 @@
-package session.eleven.models.interfaces;
-
-public interface Observable {
-    void subscribe(Observer observer);
-
-    void unSubscribe(Observer observer);
-
-    void notifyObservers();
-}
